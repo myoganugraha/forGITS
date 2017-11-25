201bf8663a5cc00d3fb47fecc9ab68d496869325
M. Yoga Nugraha <myoganugraha@gmail.com>
2017-11-25 21:40:36 +0800

    update halaman login + intent ke register

--- app/src/main/java/itk/myoganugraha/soalgits/Login.java
+++ app/src/main/java/itk/myoganugraha/soalgits/Login.java
@@ -1,13 +1,27 @@
 package itk.myoganugraha.soalgits;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button btnLinkRegist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__login);
+
+        btnLinkRegist = (Button) findViewById(R.id.btnToRegister);
+        btnLinkRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toRegisterAct = new Intent(getApplicationContext(),Register.class);
+                startActivity(toRegisterAct);
+            }
+        });
     }
 }
