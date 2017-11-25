3d920648096730547cb933f028a20498c77243f1
M. Yoga Nugraha <myoganugraha@gmail.com>
2017-11-25 19:44:03 +0800

    Initial commit

---
+++ app/src/main/java/itk/myoganugraha/soalgits/Register.java
@@ -0,0 +1,66 @@
+package itk.myoganugraha.soalgits;
+
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Register extends AppCompatActivity {
+
+    private Calendar myCalendar = Calendar.getInstance();
+    private EditText bday;
+    private DatePickerDialog.OnDateSetListener date;
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        bday.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        bday = (EditText) findViewById(R.id.bdayTxt);
+
+
+
+
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int dayOfMonth, int monthOfYear, int year) {
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.YEAR, year);
+                updateLabel();
+            }
+        };
+
+        bday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(Register.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+
+    }
+
+
+
+
+
+}
